98939f1c1c985e3927d000016dbf10dfe4044950
Javier Perez <337745+dvst@users.noreply.github.com>
2024-12-14 16:56:53 -0500

    demo backend function now return fixed json
    fixed json to be used in frontend

--- azure-function-demo/function_app.py
+++ azure-function-demo/function_app.py
@@ -1,25 +1,30 @@
 import azure.functions as func
 import logging
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    
+    accessibility_issues = [
+        {
+            "description": "Texto alternativo faltante en la imagen",
+            "code_fragment": '<img src="image.jpg">'
+        },
+        {
+            "description": "Contraste insuficiente entre el texto y el fondo",
+            "code_fragment": '<div style="color: #eee; background-color: #fff;">Texto de ejemplo</div>'
+        },
+        {
+            "description": "Elemento interactivo sin descripción significativa",
+            "code_fragment": '<button></button>'
+        }
+    ]
+    
+    return func.HttpResponse(
+        body=json.dumps(accessibility_issues),
+        mimetype="application/json",
+        status_code=200
+    )
