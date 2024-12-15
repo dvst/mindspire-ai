fa8e0a930820e04a84c223a27e88ca939e2f573f
Javier Perez <337745+dvst@users.noreply.github.com>
2024-12-14 20:58:40 -0500

    updated fixed json returned by backend
    command to update backend
    func azure functionapp publish mindspire-backenddemo

--- azure-function-demo/function_app.py
+++ azure-function-demo/function_app.py
@@ -11,15 +11,21 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     accessibility_issues = [
         {
             "description": "Texto alternativo faltante en la imagen",
-            "code_fragment": '<img src="image.jpg">'
+            "code_fragment": '<img src="image.jpg">',
+            "suggestion": "Agrega un enlace de accesibilidad a la página de inicio",
+            "code_suggestion": "<a href=\"https://www.example.com\" tabindex=\"0\">Inicio</a>",
         },
         {
             "description": "Contraste insuficiente entre el texto y el fondo",
-            "code_fragment": '<div style="color: #eee; background-color: #fff;">Texto de ejemplo</div>'
+            "code_fragment": '<div style="color: #eee; background-color: #fff;">Texto de ejemplo</div>',
+            "suggestion": "Agrega un enlace de accesibilidad a la página de inicio",
+            "code_suggestion": "<a href=\"https://www.example.com\" tabindex=\"0\">Inicio</a>",
         },
         {
             "description": "Elemento interactivo sin descripción significativa",
-            "code_fragment": '<button></button>'
+            "code_fragment": '<button></button>',
+            "suggestion": "Agrega un enlace de accesibilidad a la página de inicio",
+            "code_suggestion": "<a href=\"https://www.example.com\" tabindex=\"0\">Inicio</a>",
         }
     ]
     
