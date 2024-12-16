7a43f2a549c46945f88f2abf7a6b6859d2b42974
Leonel Breña <leonel.abd@outlook.com>
2024-12-16 12:45:51 -0500

    Filtro para página gubernamental en script endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -17,6 +17,10 @@ def analyze_page():
     except Exception as e:
         return jsonify({'second_error': 'Invalid JSON format'}), 400
 
+    # Verificar si la URL pertenece a una página gubernamental
+    if not ('.gov' in url or '.gob' in url):
+        return jsonify({'error': 'URL does not belong to a governmental website'}), 400
+        
     # 2. Ejecutar el script de análisis de accesibilidad
     try:
         hallazgos = check_wcag(url)  # Script 1
